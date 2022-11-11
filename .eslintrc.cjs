dabbf0f51159773b198964ddf41af98826875325
Cirodejesus <cjarrieta2507@hotmail.com>
2022-11-10 20:21:45 -0500

    Agrega dependencias de Eslint y configura el linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        
+     "rules": {
+        "react/react-in-jsx-scope": "off",
+         "react/prop-types": "off"
+     },
+
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+}
